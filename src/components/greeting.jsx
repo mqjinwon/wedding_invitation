67ec6840c84d2d007f86953c1b4e0eca46fdc28c
mqjinwon <mqjinwon@gmail.com>
2025-10-08 15:57:07 +0900

    fix: adjust font size in greeting component for better readability

--- src/components/greeting.jsx
+++ src/components/greeting.jsx
@@ -54,12 +54,12 @@ const Greeting = () => {
       </Content>
       <Content>
         {GROOM_FATHER_NAME} · {GROOM_MOTHER_NAME}의 아들{' '}
-        <span style={{ fontWeight: 'bold', fontSize: '1.2em' }}>
+        <span style={{ fontWeight: 'bold', fontSize: '1.1em' }}>
           {GROOM_NAME}
         </span>
         <br />
         {BRIDE_FATHER_NAME} · {BRIDE_MOTHER_NAME}의 딸{' '}
-        <span style={{ fontWeight: 'bold', fontSize: '1.2em' }}>
+        <span style={{ fontWeight: 'bold', fontSize: '1.1em' }}>
           {BRIDE_NAME}
         </span>
         <br />
